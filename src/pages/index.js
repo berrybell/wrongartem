cedf235c5e928714130555c0e997870973566c50
Olga Vorozheykina <solariskind@gmail.com>
2020-06-09 22:59:57 +0300

    Display a sample date

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,20 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import { logs } from "../components/logs"
+
 const IndexPage = () => {
-  const lastTime = null
+  const lastTime = logs[logs.length - 1]
   return (
     <Layout>
       <SEO title="Wrong Artem" />
       <h2>The last time wrong Artem was tagged in a work chat was</h2>
       <h1>
-        {lastTime ? lastTime : "some time ago (we don't know exactly when)"}
+        {lastTime
+          ? lastTime.toDateString()
+          : "some time ago (we don't know exactly when)"}
       </h1>
-      <p>In total, wrong Artem was tagged a lot of times.</p>
+      <p>In total, wrong Artem was tagged {logs.length} times.</p>
     </Layout>
   )
 }
